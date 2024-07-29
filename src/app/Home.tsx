e7f760396571bdd7d96b406b7a2df94ddd9d7af8
CHRISTABEL <obinwosugosiorah@gmail.com>
2024-07-29 00:54:07 -0500

    updated

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, LogBox } from "react-native";
 import { auth, db } from "../support/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { useRouter } from "expo-router";
 
 function Home() {
   const [firstName, setFirstname] = useState<string | null>(null);
+  const [discounts, setDiscounts] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [firstTime, setIsFirstTime] = useState(true);
   const router = useRouter();
@@ -13,9 +14,7 @@ function Home() {
   useEffect(() => {
     const fetchUserData = async () => {
       if (auth.currentUser) {
-        console.log("there is a current user");
         const userId = auth.currentUser.uid;
-        console.log("User-id = ", userId);
         const userDoc = doc(db, "users", userId);
         const docSnap = await getDoc(userDoc);
         if (docSnap.exists()) {
@@ -31,10 +30,23 @@ function Home() {
         router.push("/Login");
       }
     };
-
     fetchUserData();
   }, []);
 
+  useEffect(() => {
+    const getDiscounts = async () => {
+      try {
+        const discountRef = collection(db, "discounts");
+        const docSnapshot = await getDocs(discountRef);
+        const Discounts = docSnapshot.docs;
+        const discountList = Discounts.map((discount) => discount.data());
+        setDiscounts(discountList);
+      } catch (error) {
+        console.error("Error fetching discounts: ", error);
+      }
+    };
+    getDiscounts();
+  }, []);
   if (loading) {
     return <Text>Loading...</Text>;
   }
@@ -45,11 +57,28 @@ function Home() {
       <Text>
         {message} <Text>{firstName}</Text>
       </Text>
-
+      <Text>We do the planning, you do the eating.</Text>
       <Text>Reminder</Text>
+      <Text>Message from university</Text>
+      <Text>Latest Deals near you</Text>
       <Text>Here are the promotions available</Text>
+      {discounts.length > 0 ? (
+        discounts.map((discount, index) => (
+          <View key={index}>
+            <Text>{discount.title}</Text>
+            <Text>{discount.description}</Text>
+            <Text>{discount.discount * 100}%</Text>
+            <Text>{discount.expiryDate.toDate().toDateString()}</Text>
+          </View>
+        ))
+      ) : (
+        <Text>No available promotions</Text>
+      )}
     </View>
   );
 }
 
 export default Home;
+function atch(error: any) {
+  throw new Error("Function not implemented.");
+}
